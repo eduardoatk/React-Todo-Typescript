30060c9ff7c22ae5d4283d61baec89cb5c9b3c2e
Eduardo <eduardotk@hotmail.com>
2022-05-25 16:02:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react"
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import TaskForm from "./components/TaskForm"
+import TaskList from "./components/TaskList"
+
+import styles from "./App.module.css"
+import { ITask } from "./interfaces/Task"
+import Modal from "./components/Modal"
+
+function App() {
+	const [taskList, setTaskList] = useState<ITask[]>([])
+	const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
+
+	const deleteTask = (id: number) => {
+		setTaskList(
+			taskList.filter(task => {
+				return task.id !== id
+			})
+		)
+	}
+
+	const hideOrShowModal = (display: boolean) => {
+		const modal = document.querySelector("#modal")
+		if (display) {
+			modal!.classList.remove("hide")
+		} else {
+			modal!.classList.add("hide")
+		}
+	}
+
+	const editTask = (task: ITask) => {
+		hideOrShowModal(true)
+		setTaskToUpdate(task)
+	}
+
+	const updateTask = (id: number, title: string, difficulty: number) => {
+		const updatedTask: ITask = { id, title, difficulty }
+
+		const updatedItems = taskList.map(task => {
+			return task.id === updatedTask.id ? updatedTask : task
+		})
+
+		setTaskList(updatedItems)
+
+		hideOrShowModal(false)
+	}
+
+	return (
+		<>
+			<Modal
+				children={
+					<TaskForm
+						btnText="Editar Tarefa"
+						taskList={taskList}
+						task={taskToUpdate}
+						handleUpdate={updateTask}
+					/>
+				}
+			/>
+			<Header />
+			<main className={styles.main}>
+				<div>
+					<h2>O que você vai fazer?</h2>
+					<TaskForm
+						btnText="Criar Tarefa"
+						taskList={taskList}
+						setTaskList={setTaskList}
+					/>
+				</div>
+				<div>
+					<h2>Suas tarefas:</h2>
+					<TaskList
+						taskList={taskList}
+						handleDelete={deleteTask}
+						handleEdit={editTask}
+					/>
+				</div>
+			</main>
+			<Footer />
+		</>
+	)
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css"
+
+export default function Header() {
+	return (
+		<header className={styles.header}>
+			<h1>React + TS Todo</h1>
+		</header>
+	)
+}
